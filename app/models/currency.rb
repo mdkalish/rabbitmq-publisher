8de1a33660e1ec385ffa1d991aa2ae78a97b4729
mdkalish <mdkalish4git@gmail.com>
2015-04-07 18:47:38 +0200

    [#3] Generate unique uuid for a record before creation

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,17 @@
 class Currency < ActiveRecord::Base
+  before_validation :assign_unique_uuid
+  validates :uuid, presence: true, uniqueness: true
+
+  def self.generate_uuid
+    SecureRandom.hex
+  end
+
+  private
+
+  def assign_unique_uuid
+    loop do
+      key = self.class.generate_uuid
+      break self.uuid = key unless self.class.exists?(uuid: key)
+    end
+  end
 end
