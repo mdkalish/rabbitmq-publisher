1bd67b2368c941346d5134cf2a1636587bb51b20
mdkalish <mdkalish4git@gmail.com>
2015-04-08 15:36:31 +0200

    [#4] Fixup: change Fetcher module to class, add tests

---
+++ lib/fetcher.rb
@@ -0,0 +1,32 @@
+require 'uri'
+require 'net/http'
+require 'json'
+
+class Fetcher
+  extend ActiveSupport::Concern
+
+  BASE_URL = 'http://openexchangerates.org/api'
+  APP_ID = ENV['open_exchange_rates_app_id']
+
+  def self.fetch_currencies
+    if Currency.any? && Currency.last.created_at > 1.hour.ago
+      Currency.last
+    else
+      Currency.create!(rates: Fetcher.parsed_body['rates'])
+    end
+  end
+
+  private
+
+  def self.uri
+    URI.parse("#{BASE_URL}/latest.json?app_id=#{APP_ID}")
+  end
+
+  def self.get_response_body
+    Net::HTTP.get_response(uri).body
+  end
+
+  def self.parsed_body
+    JSON.parse(get_response_body)
+  end
+end
---
+++ spec/fetcher_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+require 'fetcher'
+require 'active_support/testing/time_helpers'
+
+describe Fetcher do
+  include ActiveSupport::Testing::TimeHelpers
+  before do
+    @rates = File.open(File.join(Dir.pwd, '/spec/fixtures/response.json')) { |l| l.read }
+    allow(Fetcher).to receive(:get_response_body).and_return(@rates)
+  end
+
+  it 'fetches rates when db is empty' do
+      expect(Currency.any?).to be_falsy
+      Fetcher.fetch_currencies
+      expect(Currency.count).to eq(1)
+    end
+
+  context 'when last fetch was less than one hour ago' do
+    it 'returns rates from the last fetch' do
+      Fetcher.fetch_currencies
+      expect(Currency.last.created_at > 1.hour.ago).to be_truthy
+      expect(Fetcher.fetch_currencies).to eq(Currency.first)
+    end
+  end
+
+  context 'when last fetch was more than one hour ago' do
+    it 'fetches new rates' do
+      travel_to 1.hour.ago
+      Fetcher.fetch_currencies
+      travel_back
+      Fetcher.fetch_currencies
+      expect(Currency.first.created_at).not_to be_within(1.hour).of(Currency.last.created_at)
+    end
+
+    it 'stores newly fetched rates in db' do
+        Fetcher.fetch_currencies
+        expect(Currency.count).to eq(1)
+        travel_to 1.hour.from_now + 1.second
+        Fetcher.fetch_currencies
+        expect(Currency.count).to eq(2)
+        travel_back
+    end
+  end
+end
