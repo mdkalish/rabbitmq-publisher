9412fe2a91d58c49b91f6532a2f7ccf6a6655fea
mdkalish <mdkalish4git@gmail.com>
2015-04-09 16:51:59 +0200

    [#4] Fixup: don't travel in time

--- spec/lib/fetcher_spec.rb
+++ spec/lib/fetcher_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 require 'fetcher'
-require 'active_support/testing/time_helpers'
 
 describe Fetcher do
-  include ActiveSupport::Testing::TimeHelpers
   let(:rates) { File.read(Rails.root.join('spec/fixtures/response.json')) }
   before { allow(Fetcher).to receive(:get_response_body).and_return(rates) }
 
@@ -29,12 +27,8 @@ describe Fetcher do
     end
 
     it 'stores newly fetched rates in db' do
-      Fetcher.fetch_currencies
-      expect(Currency.count).to eq(1)
-      travel_to 1.hour.from_now + 1.second
-      Fetcher.fetch_currencies
-      expect(Currency.count).to eq(2)
-      travel_back
+      Currency.create!(created_at: 3601.seconds.ago)
+      expect { Fetcher.fetch_currencies } .to change { Currency.count } .from(1).to(2)
     end
   end
 end
