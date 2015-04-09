ef9ea7130a5798a5f422b97fb72f5540f4dcc118
mdkalish <mdkalish4git@gmail.com>
2015-04-09 11:05:32 +0200

    [#4] Fixup: don't rely on AR last/first(), simplify rspecs, indentation

--- spec/fetcher_spec.rb
+++ spec/fetcher_spec.rb
@@ -15,28 +15,26 @@ describe Fetcher do
 
   context 'when last fetch was less than one hour ago' do
     it 'returns rates from the last fetch' do
-      Fetcher.fetch_currencies
+      initial_fetch = Fetcher.fetch_currencies
       expect(Currency.last.created_at > 1.hour.ago).to be_truthy
-      expect(Fetcher.fetch_currencies).to eq(Currency.first)
+      expect(Fetcher.fetch_currencies).to eq(initial_fetch)
     end
   end
 
   context 'when last fetch was more than one hour ago' do
     it 'fetches new rates' do
-      travel_to 1.hour.ago
-      Fetcher.fetch_currencies
-      travel_back
-      Fetcher.fetch_currencies
-      expect(Currency.first.created_at).not_to be_within(1.hour).of(Currency.last.created_at)
+      first_record = Currency.create!(created_at: 3601.seconds.ago)
+      other_record = Fetcher.fetch_currencies
+      expect(first_record.uuid).not_to eq(other_record.uuid)
     end
 
     it 'stores newly fetched rates in db' do
-        Fetcher.fetch_currencies
-        expect(Currency.count).to eq(1)
-        travel_to 1.hour.from_now + 1.second
-        Fetcher.fetch_currencies
-        expect(Currency.count).to eq(2)
-        travel_back
+      Fetcher.fetch_currencies
+      expect(Currency.count).to eq(1)
+      travel_to 1.hour.from_now + 1.second
+      Fetcher.fetch_currencies
+      expect(Currency.count).to eq(2)
+      travel_back
     end
   end
 end
