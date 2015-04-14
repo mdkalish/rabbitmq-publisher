80bd964a41817db9df194834bd8c6e52d9518d15
mdkalish <mdkalish4git@gmail.com>
2015-04-14 15:14:24 +0200

    [#7] Extra: initialize Maxretry

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -1,3 +1,14 @@
-require 'sneakers'
-Sneakers.configure({})
+require 'sneakers/handlers/maxretry'
+
+Sneakers.configure(
+  :heartbeat => 100,
+  :handler => Sneakers::Handlers::Maxretry,
+  :retry_timeout => 30_000,
+  :exchange => '',
+  :routing_key => [],
+  :workers => 1,
+  :threads => 1,
+  :prefetch => 1
+)
+
 Sneakers.logger.level = Logger::INFO
